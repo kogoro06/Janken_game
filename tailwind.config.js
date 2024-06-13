ec3a1b1b3ab49cfa5e74e435e5dfecd1dbc5a885
kogoro06 <166668414+kogoro06@users.noreply.github.com>
2024-06-13 19:00:40 +0900

    tailwinds

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    // カスタム設定
+    // 例えば、テーマの色を変更する場合など
+    // 詳細は公式ドキュメントを参照してください
+    // https://tailwindcss.com/docs/configuration
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
